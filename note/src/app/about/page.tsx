f6e06b2c90545c19c234a402eb34dc6bd840d06f
Tony <gth1123@naver.com>
2023-05-11 23:31:54 +0900

    4.3 ~ 4.6 정적 라우팅

---
+++ note/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        This app uses React, Redux, React Router, and many other helpful
+        libraries.
+      </p>
+    </div>
+  );
+}
---
+++ note/src/app/products/pants/page.tsx
@@ -0,0 +1,3 @@
+export default function PantsPage() {
+  return <div>Products/Pants Page</div>;
+}
