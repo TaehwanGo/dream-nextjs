aa42b9f3f2a8e9112b7cb8c2fde77ffb9c1231bc
Tony <gth1123@naver.com>
2023-06-11 22:27:53 +0900

    apply query param

--- blog/src/components/Categories.tsx
+++ blog/src/components/Categories.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+
 interface CategoriesProps {
   categories: string[];
   selected: string;
@@ -8,11 +11,25 @@ export default function Categories({
   selected,
   onClick,
 }: CategoriesProps) {
+  // get query param
+  const searchParams = useSearchParams();
+
+  console.log('searchParams.get("category")', searchParams.get("category"));
+
   return (
-    <section>
+    <section className="p-4 text-center">
+      <h2 className="text-lg font-bold border-b border-sky-500 md-2">
+        Categories
+      </h2>
       <ul>
         {categories.map((category) => (
-          <li key={category} onClick={() => onClick(category)}>
+          <li
+            key={category}
+            onClick={() => onClick(category)}
+            className={`cursor-pointer hover:text-sky-500 ${
+              category === searchParams.get("category") ? "text-sky-600" : ""
+            }`}
+          >
             {category}
           </li>
         ))}
--- blog/src/components/FilterablePosts.tsx
+++ blog/src/components/FilterablePosts.tsx
@@ -4,6 +4,8 @@ import { Post } from "@/service/posts";
 import { useState } from "react";
 import PostsGrid from "./PostsGrid";
 import Categories from "./Categories";
+import { useSearchParams, useRouter } from "next/navigation";
+import useQueryParams from "@/hooks/useQueryParams";
 
 interface FilterablePostsProps {
   posts: Post[];
@@ -16,18 +18,27 @@ export default function FilterablePosts({
   posts,
   categories,
 }: FilterablePostsProps) {
+  const { queryParams, setQueryParams } = useQueryParams();
   const [selected, setSelected] = useState(ALL_POSTS);
   const filtered =
     selected === ALL_POSTS
       ? posts
       : posts.filter((post) => post.category === selected);
+
   return (
-    <section>
+    <section className="flex m-4">
       <PostsGrid posts={filtered} />
       <Categories
         categories={[ALL_POSTS, ...categories]}
         selected={selected}
-        onClick={setSelected}
+        onClick={(category) => {
+          setSelected(category);
+
+          // query param
+          // window.history.pushState({}, "", `?category=${category}`);
+          // searchParams.set(category);
+          setQueryParams({ category });
+        }}
       />
     </section>
   );
