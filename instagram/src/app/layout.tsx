7e8ada8b427cd604fcb0ac4de3924ec73d4690f1
Tony <gth1123@naver.com>
2023-06-24 22:47:04 +0900

    12. 인스타그램 프로젝트 셋업

---
+++ instagram/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Instagram",
+  description: "Instagram",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={openSans.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
